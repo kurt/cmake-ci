5ccf9a2d9a098a08c0b9f00350acbddfdf663347
kurt_stewart <kurt@humaninmotion.ca>
2020-05-28 16:45:13 -0700

    Initial Commit

---
+++ build/CMakeFiles/HelloWorldC.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloWorldC.dir/hello_world.c.o"
+  "executables/HelloWorldC.pdb"
+  "executables/HelloWorldC"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HelloWorldC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
